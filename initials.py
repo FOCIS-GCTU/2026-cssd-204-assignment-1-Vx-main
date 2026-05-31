44d2061b95bcdf66412740ca0c37b29a2418bec3
Vx_Main <203350698+Vx-main@users.noreply.github.com>
2026-05-31 11:26:51 +0000

    completion

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
+# File: initials.py
+# Description: Python code to print out my initials in stylized capital large block letters.
+# Assignment Number: 1
+# 
+# Name: ISIDORE NNAJI NNAMDI
+# STUDENT ID:  2425401135
+# Email: 2425401135@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+# 
+# On my honor, ISIDORE NNAJI NNAMDI, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
+  #This is the main function for printing my initiala
+  print()
+  print("...INN")
+  print()
+  print("...IIIIIIIIIIII........NNNN....NNNN........NNNN....NNNN.....")
+  print("...IIIIIIIIIIII........NNNNN...NNNN........NNNNN...NNNN.....")
+  print(".......IIII............NNNNNN..NNNN........NNNNNN..NNNN.....")
+  print(".......IIII............NNNN.NN.NNNN........NNNN.NN.NNNN.....")
+  print(".......IIII............NNNN..NNNNNN........NNNN..NNNNNN.....")
+  print(".......IIII............NNNN...NNNNN........NNNN...NNNNN.....")
+  print(".......IIII............NNNN....NNNN........NNNN....NNNN.....")
+  print(".......IIII............NNNN....NNNN........NNNN....NNNN.....")
+  print("...IIIIIIIIIIII...**...NNNN....NNNN...**...NNNN....NNNN...**")
+  print("...IIIIIIIIIIII...**...NNNN....NNNN...**...NNNN....NNNN...**")
+  print()
 
 main()
